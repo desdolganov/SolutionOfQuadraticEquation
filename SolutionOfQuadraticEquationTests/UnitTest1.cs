82ea1dabeafc14fe0272a33cdf3d9d402d9b0442
desdolganov <desdolganov@mail.ru>
2017-09-29 17:58:13 +0300

    add unit tests

---
+++ SolutionOfQuadraticEquationTests/UnitTest1.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace SolutionOfQuadraticEquationTests
+{
+    [TestClass]
+    public class SolutionOfQuadraticEquationTests
+    {
+        [TestMethod]
+        public void stringEquation_oneStringWithThreeKoefficients()
+        {
+            string equation = "- 7 x^2 + -9x + 17 =0";
+            string exist = "-7#-9#17";
+            string result;
+            
+
+        }
+
+        [TestMethod]
+        public void stringEquation_oneStringWithThreeKoefficients2()
+        {
+            string
+        }
+
+        [TestMethod]
+        public void oneStringWithThreeKoefs_ThreeIntKoefs()
+        {
+        }
+
+        [TestMethod]
+        public void treeIntKoefs_Diskrim()
+        {
+        }
+
+        [TestMethod]
+        public void diskrim_diskrimShouldbepositive()
+        {
+        }
+
+        [TestMethod]
+        public void discrimAndThreeKoefs_FirstRootShouldBeRight()
+        {
+        }
+
+        [TestMethod]
+        public void discrimAndThreeKoefs_SecondRootShouldBeRight()
+        {
+        }
+    }
+}
